80f32efeb91a4172f5cc8d09bd9b43e3f51fa35e
Nathan Peek <nmp14fsu@gmail.com>
2021-05-06 22:08:01 -0400

    Add sequelize connection and express setup

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const path = require("path");
+const sequelize = require("./connection");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const sess = {
+    secret: "Pubs and Peeps secret items",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
+app.use(session(sess));
+
+// Express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
