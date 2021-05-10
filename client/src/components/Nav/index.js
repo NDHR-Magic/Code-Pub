a18f0dcee351bca890229d7b886f638d8039ed28
Nathan Peek <nmp14fsu@gmail.com>
2021-05-09 22:53:51 -0400

    Create nav bar elements and style. Use react-media to determine screen size

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Media from "react-media";
+import "./style.css";
 
 function Nav() {
     return (
         <nav>
+            <Media queries={{ small: { maxWidth: 800 } }}>
+                {matches =>
+                    matches.small ? (
+                        <div id="small-nav" className="nav-bar flex-between">
+                            <div className="nav-left flex-between flex-align">
+                                <h1>Logo</h1>
+                            </div>
 
+                            <div className="nav-right flex-end flex-align">
+                                <i className="fas small fa-bars"></i>
+                            </div>
+                        </div>
+                    ) : (
+                        <div id="wide-nav" className="nav-bar flex-between">
+                            <div className="nav-left flex-between flex-align">
+                                <h1>Logo</h1>
+                                <Link to="#">Menu</Link>
+                                <Link to="#">Events</Link>
+                                <Link to="#">Store</Link>
+                                <Link to="#">Drink App</Link>
+                            </div>
+
+                            <div className="nav-right flex-between flex-align">
+                                <Link to="#" className="nav-link custom-flex flex-align">Login</Link>
+                                <div className="cart">
+                                    <i className="fas fa-shopping-cart wide"></i>
+                                    <div className="cart-count custom-flex flex-align">0</div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </Media>
         </nav>
     )
 }
