122926f10d6a64885731d94e554ec7cebca4a298
Nathan Peek <nmp14fsu@gmail.com>
2021-05-09 23:07:05 -0400

    Add routes for about, cart, drinkingapp, events, login, and menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,44 @@ import React from "react";
 import { BrowserRouter as TheCoolestRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
+import Menu from "./pages/Menu";
+import Events from "./pages/Events";
+import DrinkApp from "./pages/DrinkApp";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <TheCoolestRouter>
       <Nav />
+
       <Route exact path="/">
         <Home />
       </Route>
-      <Route exact path="/merch">
+
+      <Route exact path="/store">
         <Store />
+      </Route>
+
+      <Route exact path="/menu">
+        <Menu />
+      </Route>
+
+      <Route exact path="/events">
+        <Events />
+      </Route>
+
+      <Route exact path="/mixer">
+        <DrinkApp />
+      </Route>
+
+      <Route exact path="/cart">
+        <Cart />
+      </Route>
+
+      <Route exact path="/login">
+        <Login />
       </Route>
     </TheCoolestRouter>
   );
