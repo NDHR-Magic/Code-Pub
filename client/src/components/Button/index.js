b36495480f2dcfeab6fd47c83d00f1a78e8c359b
Nathan Peek <nmp14fsu@gmail.com>
2021-05-10 04:30:35 -0400

    Create button component with dynamic bg and text color

---
+++ client/src/components/Button/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Button = (props) => {
+    return (
+        <button className="btn" style={{
+            backgroundColor: props.bg ? props.bg : "#0275d8",
+            color: props.text_color ? props.text_color : "#fff"
+        }}>{props.children}</button>
+    )
+}
+
+export default Button;
