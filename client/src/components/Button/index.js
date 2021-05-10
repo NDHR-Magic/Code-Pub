281b0bd70cbd678bba1acdbaa4acf3f0c36228bb
Nathan Peek <nmp14fsu@gmail.com>
2021-05-10 05:33:50 -0400

    Add dyanmic onClick functions and style the cursor:pointer

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -3,10 +3,12 @@ import "./style.css";
 
 const Button = (props) => {
     return (
-        <button className="btn" style={{
-            backgroundColor: props.bg ? props.bg : "#0275d8",
-            color: props.text_color ? props.text_color : "#fff"
-        }}>{props.children}</button>
+        <button
+            onClick={(e) => props.onClickEvent(e)}
+            className="btn" style={{
+                backgroundColor: props.bg ? props.bg : "#0275d8",
+                color: props.text_color ? props.text_color : "#fff"
+            }}>{props.children}</button>
     )
 }
 
