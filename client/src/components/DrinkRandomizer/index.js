76d3b9399b0a9637f11cd32d0fd4ccea27ca1736
Nathan Peek <nmp14fsu@gmail.com>
2021-05-11 04:29:38 -0400

    Start drinkrandomizer

--- client/src/components/DrinkRandomizer/index.js
+++ client/src/components/DrinkRandomizer/index.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Header from "../Header";
+import Button from "../Button";
+import "./style.css";
 
 const DrinkRandomizer = () => {
     return (
-        <div>
-            Randomizer
+        <div className="drink-api">
+            <Header><h1>Partying at Home?</h1></Header>
+
+            <form>
+                <label htmlFor="alcohol">Choose an alcohol type </label>
+                <select name="alcohol" id="alcohol">
+                    <option value="tequila">Tequila</option>
+                    <option value="rum">Rum</option>
+                    <option value="beer">Beer</option>
+                </select>
+                <div className="custom-flex">
+                    <Button>Get random drink!</Button>
+                </div>
+            </form>
         </div>
     )
 }
