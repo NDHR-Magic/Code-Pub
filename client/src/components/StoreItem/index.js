a4f815524073b97e6488e4ec219b902eb19f895c
Nathan Peek <nmp14fsu@gmail.com>
2021-05-13 21:10:28 -0400

    Style store items on store page. Add some to seeds files

--- client/src/components/StoreItem/index.js
+++ client/src/components/StoreItem/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Button from "../Button";
 
-function StoreItem (props) {
-    return(
-        <div className="row mt-4">
-            <div className="col-4">
+function StoreItem({ itemName, itemImg, itemPrice }) {
+    return (
+        <div className="col-lg-4 col-sm-6">
             <div className="card">
                 <div className="card-header">
-                    <h1 className="fs-2">Item Name</h1>
+                    <h1 className="fs-2">{itemName}</h1>
                 </div>
                 <div className="card-body">
-                    <img src="https://www.loveyourdog.com/wp-content/uploads/2020/04/Chihuahua-Relaxing-Indoors.jpg" height="100" width="100"></img>
-                    <h3>$19.99</h3>
+                    <img src={itemImg} alt={itemName}></img>
+                    <h3>{`$${itemPrice}`}</h3>
+
+                    <div className="custom-flex" style={{ width: "100%" }}>
+                        <Button>Add to cart</Button>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     )
