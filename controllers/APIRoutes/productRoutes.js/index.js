e948db7ac5e5d3d651494c81d1ce395ae14640bb
Nathan Peek <nmp14fsu@gmail.com>
2021-05-16 06:54:46 -0400

    Create get route for specific product

--- controllers/APIRoutes/productRoutes.js/index.js
+++ controllers/APIRoutes/productRoutes.js/index.js
@@ -12,6 +12,19 @@ router.get("/", async (req, res) => {
         res.status(500).json(e);
         console.log(e)
     }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const productData = await Item.findByPk(req.params.id);
+
+        const product = productData.get({ plain: true });
+
+        res.status(200).json(product);
+    } catch (e) {
+        res.status(500).json(e);
+        console.log(e);
+    }
 })
 
 module.exports = router;
