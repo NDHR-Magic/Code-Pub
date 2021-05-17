c6b04a5303c359a397e173724c99a74d7bb80aae
Nathan Peek <nmp14fsu@gmail.com>
2021-05-17 19:49:46 -0400

    Create register api call

--- client/src/utils/UserAPI.js
+++ client/src/utils/UserAPI.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export const signInRequest = async (email, password) => {
     return await axios.post("/api/users/login", { email, password });
+};
+
+export const registerRequest = async (first_name, last_name, username, email, password) => {
+    return await axios.post("/api/users/register", { first_name, last_name, username, email, password });
 }
