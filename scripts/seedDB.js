8d28dac3f13873ce04673c29553b69d916fc1092
Nathan Peek <nmp14fsu@gmail.com>
2021-05-17 02:56:33 -0400

    Remove age from user model and create user seeds

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,13 +1,17 @@
 const sequelize = require("../config/connection");
-const {Item, User} = require("../models");
+const { Item, User } = require("../models");
 const itemData = require("./seed-storeItems.json");
+const userData = require("./user-seeds.json");
 
 const seedAll = async () => {
     try {
-        await sequelize.sync({force: true});
+        await sequelize.sync({ force: true });
 
         await Item.bulkCreate(itemData);
         console.log("Seeded items \n");
+
+        await User.bulkCreate(userData);
+        console.log("Seeded users\n");
 
         process.exit(0);
     } catch (e) {
