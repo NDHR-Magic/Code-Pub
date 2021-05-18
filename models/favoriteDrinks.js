4a2d61dba8830c92275331afeb119b2afd5270d3
Rachel Diaz <Rachel7113@gmail.com>
2021-05-17 20:38:58 -0400

    Edited favoriteDrink model

--- models/favoriteDrinks.js
+++ models/favoriteDrinks.js
@@ -1,11 +1,37 @@
 const sequelize = require('../connection');
-const { Model, Datatypes } = require('sequelize');
+const { Model, Datatypes, DataTypes } = require('sequelize');
 
-class favoriteDrinks extends Model {}
+class favoriteDrinks extends Model { }
 
-favoriteDrinks.init (
+favoriteDrinks.init(
     {
-
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'favoriteDrinks',
     }
 )
 
