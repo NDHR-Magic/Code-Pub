27995d45fffe2bda95144584d4f216a6f2d5b43c
Nathan Peek <nmp14fsu@gmail.com>
2021-05-18 00:55:31 -0400

    Add ssave shipping address constant and action to cart reducer and store

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
 
 // Reducer to add items to cart
 export const cartReducer = (state = { cartItems: [] }, action) => {
@@ -16,6 +16,8 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
             } else {
                 return { ...state, cartItems: [...state.cartItems, item] };
             }
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return { ...state, shippingAddress: action.payload }
         case CART_REMOVE_ITEM:
             return { ...state, cartItems: state.cartItems.filter(item => item.product !== action.payload) }
         default:
