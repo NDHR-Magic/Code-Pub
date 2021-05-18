03d77b59b27cd0832a679726c8bc0fc7cbc5e2b8
Nathan Peek <nmp14fsu@gmail.com>
2021-05-18 00:57:44 -0400

    Change export method

--- client/src/utils/CocktailApi/index.js
+++ client/src/utils/CocktailApi/index.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-export default {
-    searchDrink: async function (drinkType) {
-        const results = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${drinkType}`);
-        return results;
-    },
+export const searchDrink = async (drinkType) => {
+    const results = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${drinkType}`);
+    return results;
+};
 
-    searchDrinkInfo: async function (drink) {
-        const results = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-        return results;
-    }
+export const searchDrinkInfo = async (drink) => {
+    const results = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    return results;
 };
