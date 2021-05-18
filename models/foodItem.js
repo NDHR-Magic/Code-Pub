b90639e0d2da12cd04c9b5c4adb40127d8a5f152
Rachel Diaz <Rachel7113@gmail.com>
2021-05-17 20:39:54 -0400

    Added foodItem model

---
+++ models/foodItem.js
@@ -0,0 +1,44 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class foodItem extends Model {
+
+};
+
+foodItem.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'foodItem',
+    }
+)
+
+module.exports = foodItem;
