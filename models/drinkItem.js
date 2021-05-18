c42fc981cba3f07768be8cd9095b12ff66f3057e
Rachel Diaz <Rachel7113@gmail.com>
2021-05-17 20:39:28 -0400

    Added drinkItem model

---
+++ models/drinkItem.js
@@ -0,0 +1,44 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class drinkItem extends Model {
+
+};
+
+drinkItem.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: true,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'drinkItem',
+    }
+)
+
+module.exports = drinkItem;
