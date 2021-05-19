f6794d5df3fd6b6932e6b072565ef2f13bb90f4c
Rachel Diaz <Rachel7113@gmail.com>
2021-05-18 21:51:03 -0400

    Added seed script for food & drink items

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,11 +1,19 @@
 const sequelize = require("../config/connection");
-const { Item, User } = require("../models");
+const { Item, User, drinkItem, foodItem } = require("../models");
 const itemData = require("./seed-storeItems.json");
 const userData = require("./user-seeds.json");
+const drinkData = require("./drink-seeds.json");
+const foodData = require("./food-seeds.json");
 
 const seedAll = async () => {
     try {
         await sequelize.sync({ force: true });
+
+        await drinkItem.bulkCreate(drinkData);
+        console.log("Seeded drinks \n");
+
+        await foodItem.bulkCreate(foodData);
+        console.log("Seeded food items \n");
 
         await Item.bulkCreate(itemData);
         console.log("Seeded items \n");
