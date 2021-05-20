b93de3cd2e005b9bfff07574a6f5df56847284ac
HaderCore <Hadercortes@gmail.com>
2021-05-19 21:32:01 -0400

    created sequelize  event models

---
+++ models/Events.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Events extends Model { }
+
+Events.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    event_date: {
+        type:DataTypes.DATE,
+        allowNull: true
+    }, 
+    
+    
+
+}, {
+    sequelize,
+    timestamps:true,
+    modelName:"Event",
+    freezeTableName: true,
+});
+
+module.exports = Events;
