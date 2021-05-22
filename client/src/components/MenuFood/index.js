97a619f9139c55ef4d4a79a855c7be40f1c3266f
Rachel Diaz <Rachel7113@gmail.com>
2021-05-21 23:00:31 -0400

    Added components for drinks and food

---
+++ client/src/components/MenuFood/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const MenuFood = (props) => {
+
+    return (
+        <div className="foodHeaders">
+            <div className="appsTitle col">Starters</div>
+            <div className="mainCourse col">Main Dishes</div>
+        </div>
+    )
+}
+
+export default MenuFood;
