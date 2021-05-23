81947ffcc894fbd84c1000c733ec41889bae73c0
fidel bello <59423@bears.smcc.edu>
2021-05-23 14:49:23 -0400

    created profile page skeleton  that has user favorite meals and favorite drinks

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -2,11 +2,33 @@ import React from 'react';
 
 
 
-class Profile extends React.Component {
-    render() {
-        return(
-            <div>hey</div>
-        )
-    }
+function Profile() {
+    return (
+        <div className="Wrapper">
+            <div className="left">
+                <img src="https://via.placeholder.com/250" alt="user" width="100"></img>
+                <h4>User Name</h4>
+                <h5>First and Last</h5>
+            </div>
+            <div className="right">
+                <div className="info">
+                    <h3> Favorite Drinks</h3>
+                    <div className="info-data"></div>
+                    <div className='data'>
+                    <img src="https://via.placeholder.com/250" alt='drink' width='70'></img>
+                    </div>
+                </div>
+                <div className="meals">
+                    <h3>Favorite meals</h3>
+                    <div className="favMealsData">
+                        <div className="data">
+                        <img src="https://via.placeholder.com/250" alt='meal' width='70'></img> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
+
 export default Profile;
