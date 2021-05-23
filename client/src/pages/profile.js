3473bbfb3e6202dbff621f657c154bb736333cf3
fidel bello <59423@bears.smcc.edu>
2021-05-23 15:11:28 -0400

    finished left side styling

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%,-50%);
+width: 450px;
+display: flex;
+box-shadow: 0 1px 20px 0 rgba(69,90,100,.08);
+`; 
 
+const Left = styled.div`
+width: 35%;
+background: -moz-linear-gradient(90deg, rgba(0,128,128,1) 15%, rgba(103,179,179,1) 68%, rgba(255,255,255,1) 100%);
+background: -webkit-linear-gradient(90deg, rgba(0,128,128,1) 15%, rgba(103,179,179,1) 68%, rgba(255,255,255,1) 100%);
+background: linear-gradient(90deg, rgba(0,128,128,1) 15%, rgba(103,179,179,1) 68%, rgba(255,255,255,1) 100%);
+padding: 30px 25px;
+border-top-left-radius: 5px;
+border-bottom-left-radius: 5px;
+text-align: center;
+color: #fff;
+`
 
+const Right = styled.div`
+
+`
 function Profile() {
     return (
-        <div className="Wrapper">
-            <div className="left">
+       <Wrapper>
+            <Left>
                 <img src="https://via.placeholder.com/250" alt="user" width="100"></img>
                 <h4>User Name</h4>
                 <h5>First and Last</h5>
-            </div>
+            </Left>
             <div className="right">
                 <div className="info">
                     <h3> Favorite Drinks</h3>
@@ -27,7 +51,7 @@ function Profile() {
                     </div>
                 </div>
             </div>
-        </div>
+       </Wrapper>
     )
 }
 
