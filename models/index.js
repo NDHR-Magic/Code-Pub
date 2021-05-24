dbb9df34f33a0f1c2e26113af1f5a084345f7982
Nathan Peek <nmp14fsu@gmail.com>
2021-05-23 22:56:46 -0400

    Fix naming

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const Item = require("./item");
 const User = require("./user");
 const Cart = require("./Cart");
 const drinkItem = require("./drinkItem");
-const favoriteDrinks = require("./favoriteDrinks");
+const FavoriteDrinks = require("./favoriteDrinks");
 const foodItem = require("./foodItem");
 const Events = require("./Events");
 const Order = require("./Orders");
@@ -10,18 +10,19 @@ const OrderItem = require("./OrderItem");
 const ShippingAddress = require("./ShippingAddress");
 const PaymentResult = require("./PaymentResult");
 
-User.hasMany(favoriteDrinks, {
-    foreignKey: "user_id"
+User.hasMany(FavoriteDrinks, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
 });
 
-favoriteDrinks.belongsTo(User, {
+FavoriteDrinks.belongsTo(User, {
     foreignKey: "user_id"
 });
 
 User.hasMany(Order, { foreignKey: "user_id" });
 Order.belongsTo(User, { foreignKey: "user_id" });
 
-Order.hasMany(OrderItem, { foreignKey: "order_id" });
+Order.hasMany(OrderItem, { foreignKey: "order_id", onDelete: "CASCADE" });
 OrderItem.belongsTo(Order, { foreignKey: "order_id" });
 
 Item.hasMany(OrderItem, { foreignKey: "item_id" });
@@ -30,7 +31,7 @@ OrderItem.belongsTo(Item, { foreignKey: "item_id" });
 Order.hasOne(ShippingAddress, { foreignKey: "order_id" });
 ShippingAddress.belongsTo(Order, { foreignKey: "order_id" });
 
-Order.hasOne(PaymentResult, { foreignKey: "order_id" });
+Order.hasOne(PaymentResult, { foreignKey: "order_id", onDelete: "CASCADE" });
 PaymentResult.belongsTo(Order, { foreignKey: "order_id" });
 
-module.exports = { Item, User, Cart, drinkItem, favoriteDrinks, foodItem, Events, Order, OrderItem, ShippingAddress, PaymentResult };
+module.exports = { Item, User, Cart, drinkItem, FavoriteDrinks, foodItem, Events, Order, OrderItem, ShippingAddress, PaymentResult };
