39351efdc98c5bbaebcc18c8286799005ac3e90f
Nathan Peek <nmp14fsu@gmail.com>
2021-05-25 00:45:30 -0400

    Add isAdmin authentication

--- utils/utils.js
+++ utils/utils.js
@@ -27,6 +27,14 @@ const isAuth = (req, res, next) => {
     } else {
         res.status(401).json({ message: "No Token" });
     }
-}
+};
+
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401).json({ message: "No Token" });
+    }
+};
 
 module.exports = { generateToken, isAuth };
