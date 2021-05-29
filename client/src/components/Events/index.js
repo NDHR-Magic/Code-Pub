dd45ae898fa17830aed81b515a5c41b08981608c
David Sanchez <davidmsanchez250@gmail.com>
2021-05-28 21:53:50 -0400

    added styling to events. Created event seeds.

--- client/src/components/Events/index.js
+++ client/src/components/Events/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./style.css";
 
 const Event = props => {
     return (
-        <div>
-            <div className="card mt-4">
+        <div className='eventContainer'>
+            <div className="card mb-5 bg-dark">
                 <Link to={`/events/${props.id}`}>
-                    <div className="card-header">
+                    <div className="card-header text-white">
                         <h2>{props.title}</h2>
                     </div>
                 </Link>
-                <div className="card-body">
+                <div className="card-body p-5 text-white">
                     <p className="card-text">{props.desc}</p>
                 </div>
             </div>
