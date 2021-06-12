6bb19bf98284375f1413075c12a15c66906a0e01
Nathan Peek <nmp14fsu@gmail.com>
2021-06-12 03:49:41 -0400

    Add more to test

--- client/src/components/DrinkRandomizer/DrinkRandomizer.test.js
+++ client/src/components/DrinkRandomizer/DrinkRandomizer.test.js
@@ -1,25 +1,37 @@
 // Test the Home page component in src/pages
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { toBeInTheDocument } from "@testing-library/jest-dom";
 import userEvent from '@testing-library/user-event';
 
 import DrinkRandomizer from "./index";
 
 describe("Drink randomizer app", () => {
     test("It should render the form and test", () => {
-        render(<DrinkRandomizer />);
+        const { getByText, getByRole } = render(<DrinkRandomizer />);
 
-        screen.getByText("Not sure what to order at the bar?")
-        screen.getByRole("combobox");
+        getByText("Not sure what to order at the bar?")
+        getByRole("combobox");
         screen.getAllByRole("option");
         const img = screen.getByRole("img");
         // Part of the url for mojito img from cocktail db and text
         expect(img.src).toContain("metwgh1606770327.jpg");
-        screen.getByText("Mojito");
-        screen.getByText(/Muddle mint leaves/);
+        getByText("Mojito");
+        getByText(/Muddle mint leaves/);
+
+        const refFn = jest.spyOn(React, "useRef");
+        refFn.mockImplementation(() => {
+            return getByRole('combobox').value;
+        });
+
+        userEvent.selectOptions(getByRole('combobox'), ['gin']);
+
+        const ref = refFn();
+        expect(ref).toBe("gin");
+
     });
 
-    test("When button is clicked, it calls a function to query the cocktail db API", () => {
+    test("When button is clicked, it calls a function to query the cocktail db API", async () => {
         render(<DrinkRandomizer />);
 
         // Select gin option of select div and then click button.
