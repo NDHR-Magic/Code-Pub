3a0950bc1f2da8f0133a80460c1b71aa4ab90435
Nathan Peek <nmp14fsu@gmail.com>
2021-06-13 04:06:08 -0400

    Create tests for menu actions and reducers

--- client/src/__test__/MenuActionsReducers.test.js
+++ client/src/__test__/MenuActionsReducers.test.js
@@ -80,13 +80,28 @@ describe("Create menu item action", () => {
             })
         });
 
-        const cb = menuActions.createMenuItem({}, "food");
+        let cb = menuActions.createMenuItem({}, "food");
         await cb.call(this, dispatch, getState);
 
         expect(dispatch.mock.calls[0]).toEqual([{ type: MENU_CREATE_ITEM_REQUEST }]);
         expect(dispatch.mock.calls[1]).toEqual([{
             type: MENU_CREATE_ITEM_SUCCESS, payload: {
                 name: "React Burger", price: 19.22
+            }
+        }]);
+
+        dispatch.mockClear();
+
+        // make sure call works with drink argument passed. Didnt change return info but thats ok
+        cb = menuActions.createMenuItem({}, "drink");
+        await cb.call(this, dispatch, getState);
+
+        expect(dispatch.mock.calls[0]).toEqual([{ type: MENU_CREATE_ITEM_REQUEST }]);
+        expect(dispatch.mock.calls[1]).toEqual([{
+            type: MENU_CREATE_ITEM_SUCCESS, payload: {
+                foodData: {
+                    name: "React Burger", price: 19.22
+                }
             }
         }]);
     });
@@ -123,7 +138,17 @@ describe("Menu delete action", () => {
     it("Requests to delete and item and returns successfully", async () => {
         axios.delete.mockResolvedValue({ message: "Succesfully deleted item" });
 
-        const cb = menuActions.deleteMenuItem(1, "food");
+        let cb = menuActions.deleteMenuItem(1, "food");
+        await cb.call(this, dispatch, getState);
+
+        expect(dispatch.mock.calls[0]).toEqual([{ type: DELETE_MENU_ITEM_REQUEST }]);
+        expect(dispatch.mock.calls[1]).toEqual([{ type: DELETE_MENU_ITEM_SUCCESS, payload: undefined }]);
+        expect(dispatch.mock.calls[2]).toEqual([{ type: MENU_ITEM_RESET }]);
+
+        // Test it also works with drink argument
+        dispatch.mockClear();
+
+        cb = menuActions.deleteMenuItem(1, "drink");
         await cb.call(this, dispatch, getState);
 
         expect(dispatch.mock.calls[0]).toEqual([{ type: DELETE_MENU_ITEM_REQUEST }]);
@@ -141,5 +166,83 @@ describe("Menu delete action", () => {
 
         expect(dispatch.mock.calls[0]).toEqual([{ type: DELETE_MENU_ITEM_REQUEST }]);
         expect(dispatch.mock.calls[1]).toEqual([{ type: DELETE_MENU_ITEM_FAIL, payload: "Error" }]);
+    });
+});
+
+describe("Menu get reducer", () => {
+    const initialState = {};
+    const menuItems = {
+        food: [{ name: "React Burger", price: 9.99 }],
+        drink: [{ name: "Mojito" }]
+    };
+
+    it("Should return the initial state", () => {
+        expect(menuReducers.menuGetReducer(initialState, {})).toEqual(initialState);
+    });
+
+    it("Should should correctly return states on request, success, fail, and reset", () => {
+        jest.spyOn(localStorage, "removeItem");
+
+        expect(menuReducers.menuGetReducer(initialState, { type: MENU_ITEM_REQUEST })).toEqual({
+            ...initialState, loading: true
+        });
+
+        expect(menuReducers.menuGetReducer(initialState, { type: MENU_ITEM_SUCCESS, payload: menuItems })).toEqual({
+            ...initialState, loading: false, menu: menuItems
+        });
+
+        expect(menuReducers.menuGetReducer(initialState, { type: MENU_ITEM_FAIL, payload: "Error" })).toEqual({
+            ...initialState, loading: false, error: "Error"
+        });
+
+        expect(menuReducers.menuGetReducer(menuItems, { type: MENU_ITEM_RESET })).toEqual(initialState);
+    });
+});
+
+describe("Menu create reducer", () => {
+    const initialState = {};
+    const menuItem = {
+        name: "React Burger", price: 9.99
+    };
+
+    it("Should return default state on wrong dispatch", () => {
+        expect(menuReducers.createMenuItemReducer(initialState, {})).toEqual(initialState);
+    });
+
+    it("Should return state corrently on request, success, and fail", () => {
+        expect(menuReducers.createMenuItemReducer(initialState, { type: MENU_CREATE_ITEM_REQUEST })).toEqual({
+            ...initialState, loading: true
+        });
+
+        expect(menuReducers.createMenuItemReducer(initialState, { type: MENU_CREATE_ITEM_SUCCESS, payload: menuItem })).toEqual({
+            ...initialState, loading: false, menuItem
+        });
+
+        expect(menuReducers.createMenuItemReducer(initialState, { type: MENU_CREATE_ITEM_FAIL, payload: "Error" })).toEqual({
+            ...initialState, loading: false, error: "Error"
+        });
+    });
+});
+
+describe("Menu delete reducer", () => {
+    const initialState = {};
+    const deletedItem = 1;
+
+    it("Should return the default state on wrong dispatch type", () => {
+        expect(menuReducers.deleteMenuItemReducer(initialState, {})).toEqual(initialState);
+    });
+
+    it("Should return the state correctly on requests, success, and fails", () => {
+        expect(menuReducers.deleteMenuItemReducer(initialState, { type: DELETE_MENU_ITEM_REQUEST })).toEqual({
+            ...initialState, loading: true
+        });
+
+        expect(menuReducers.deleteMenuItemReducer(initialState, { type: DELETE_MENU_ITEM_SUCCESS, payload: deletedItem })).toEqual({
+            ...initialState, loading: false, deletedItem
+        });
+
+        expect(menuReducers.deleteMenuItemReducer(initialState, { type: DELETE_MENU_ITEM_FAIL, payload: "Error" })).toEqual({
+            ...initialState, loading: false, error: "Error"
+        });
     });
 });
