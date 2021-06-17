3f14d88598d0a6db265fe0c9351f632ce6bd5761
Nathan Peek <nmp14fsu@gmail.com>
2021-06-17 17:31:00 -0400

    Create tests for product actions

--- client/src/__test__/ProductActionsReducers.test.js
+++ client/src/__test__/ProductActionsReducers.test.js
@@ -1,6 +1,9 @@
 import configureMockStore from "redux-mock-store";
 import axios from "axios";
 import thunk from "redux-thunk";
+import * as productActions from "../actions/productActions";
+import * as productReducers from "../reducers/productReducers";
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
 
 jest.mock("axios");
 
@@ -9,10 +12,64 @@ const mockStore = configureMockStore(middlewares);
 
 let store;
 beforeEach(() => {
-    store = mockStore({ userInfo: {} });
-    axios.post.mockClear();
+    store = mockStore({});
+    axios.mockClear();
 });
 
 describe("Get all products action", () => {
+    it("Should request and receive data then dispatch success event", async () => {
+        axios.mockResolvedValue({
+            data: [{ name: "React Shot Glass", price: 12.99, amount_in_stock: 10 }, { name: "T-shirt", price: 8.99, amount_in_stock: 0 }]
+        });
 
+        await store.dispatch(productActions.getAllProducts());
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({ type: PRODUCT_LIST_REQUEST });
+        expect(actions[1]).toEqual({ type: PRODUCT_LIST_SUCCESS, payload: [{ name: "React Shot Glass", price: 12.99, amount_in_stock: 10 }, { name: "T-shirt", price: 8.99, amount_in_stock: 0 }] })
+        expect(actions.length).toBe(2);
+    });
+
+    it("Should request and send error message on failed request", async () => {
+        axios.mockImplementation(() => {
+            return Promise.reject({ message: "This is a jest error test" });
+        });
+
+        await store.dispatch(productActions.getAllProducts());
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({ type: PRODUCT_LIST_REQUEST });
+        expect(actions[1]).toEqual({ type: PRODUCT_LIST_FAIL, payload: "This is a jest error test" });
+        expect(actions.length).toBe(2);
+    });
+});
+
+describe("Get single product action", () => {
+    it("Should request and receive single data object and then dispatch success event", async () => {
+        axios.mockResolvedValue({ data: { id: 1, name: "Jest Shot Glass", price: 999.99, amount_in_stock: 999 } });
+
+        await store.dispatch(productActions.getProduct(1));
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({ type: PRODUCT_DETAILS_REQUEST, payload: 1 });
+        expect(actions[1]).toEqual({ type: PRODUCT_DETAILS_SUCCESS, payload: { id: 1, name: "Jest Shot Glass", price: 999.99, amount_in_stock: 999 } });
+        expect(actions.length).toBe(2);
+    });
+
+    it("Should request and send error message on failed event", async () => {
+        axios.mockImplementation(() => {
+            return Promise.reject({ message: "Another jest error message" });
+        });
+
+        await store.dispatch(productActions.getProduct(1));
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({ type: PRODUCT_DETAILS_REQUEST, payload: 1 });
+        expect(actions[1]).toEqual({ type: PRODUCT_DETAILS_FAIL, payload: "Another jest error message" });
+        expect(actions.length).toBe(2);
+    });
 });
